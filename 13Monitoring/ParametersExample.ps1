a502eef9b8035fbb02570ede5e54a55cc0e369de
Djkormo <djkormo@gmail.com>
2019-01-01 18:56:36 +0100

    Adding pictures for week 13

---
+++ 13Monitoring/ParametersExample.ps1
@@ -0,0 +1,25 @@
+Configuration ParametersExample
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $FeatureName,
+
+        [Parameter(Mandatory=$true)]
+        [boolean] $IsPresent
+    )
+
+    $EnsureString = 'Present'
+    if($IsPresent -eq $false)
+    {
+        $EnsureString = 'Absent'
+    }
+
+    Node 'sample'
+    {
+        WindowsFeature ($FeatureName + 'Feature')
+        {
+            Ensure = $EnsureString
+            Name   = $FeatureName
+        }
+    }
+}
