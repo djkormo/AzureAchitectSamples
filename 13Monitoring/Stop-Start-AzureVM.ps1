95f3288be9abb08b244b73aa9a7fdf3a68a1cd65
Djkormo <djkormo@gmail.com>
2019-01-01 23:33:44 +0100

    Adding content for Automation solution for reseeting web server week 13

--- 13Monitoring/Stop-Start-AzureVM.ps1
+++ 13Monitoring/Stop-Start-AzureVM.ps1
@@ -4,18 +4,53 @@ Workflow Stop-Start-AzureVM
     (    
         [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
         [String] 
-        $AzureSubscriptionId, 
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
-        [String] 
         $AzureVMList="All", 
-        [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")] 
+        [Parameter(Mandatory=$true)][ValidateSet("Start","Stop","Reset")] 
         [String] 
-        $Action 
+        $Action ,
+        [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $ScriptName
     ) 
      
-    $credential = Get-AutomationPSCredential -Name 'AzureCredential' 
-    Login-AzureRmAccount -Credential $credential 
-    Select-AzureRmSubscription -SubscriptionId $AzureSubscriptionId 
+     ## Authentication
+    Write-Output ""
+    Write-Output "------------------------ Authentication ------------------------"
+    Write-Output "Logging in to Azure ..."
+
+    try
+    {
+        $connectionName = "AzureRunAsConnection"
+
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+        $null = Add-AzureRmAccount `
+                    -ServicePrincipal `
+                    -TenantId $servicePrincipalConnection.TenantId `
+                    -ApplicationId $servicePrincipalConnection.ApplicationId `
+                    -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+        Write-Output "Successfully logged in to Azure." 
+    } 
+    catch
+    {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } 
+        else
+        {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+    ## End of authentication
+
+    #$credential = Get-AutomationPSCredential -Name 'AzureCredential' 
+    #Login-AzureRmAccount -Credential $credential 
+    #Select-AzureRmSubscription -SubscriptionId $AzureSubscriptionId 
  
     if($AzureVMList -ne "All") 
     { 
@@ -39,18 +74,46 @@ Workflow Stop-Start-AzureVM
  
     if($Action -eq "Stop") 
     { 
+        Write-Output "------------------------ Stopping VM BEGIN ------------------------"
         Write-Output "Stopping VMs"; 
         foreach -parallel ($AzureVM in $AzureVMsToHandle) 
         { 
+            Write-Output " Stopping AzureVM : [$AzureVM]  " 
             Get-AzureRmVM | ? {$_.Name -eq $AzureVM} | Stop-AzureRmVM -Force 
         } 
+        Write-Output "------------------------ Stopping VM END ------------------------"
     } 
-    else 
-    { 
+    if ($Action -eq "Start")
+    {
+        Write-Output "------------------------ Starting VM BEGIN ------------------------"
         Write-Output "Starting VMs"; 
         foreach -parallel ($AzureVM in $AzureVMsToHandle) 
         { 
+            Write-Output " Starting AzureVM : [$AzureVM]  " 
             Get-AzureRmVM | ? {$_.Name -eq $AzureVM} | Start-AzureRmVM 
+        }  
+        Write-Output "------------------------ Starting VM END ------------------------"
+    }
+    # Resetting 
+    if ($Action -eq "Reset")
+    { 
+        Write-Output "------------------------ Resetting VM BEGIN ------------------------"
+        Write-Output "Downloading script for resetting services on VM"; 
+        $FullScriptName= "https://raw.githubusercontent.com/djkormo/AzureAchitectSamples/master/13Monitoring/$ScriptName"
+        Write-Output "Full script name : $FullScriptName"
+        wget $FullScriptName -outfile $ScriptName
+
+        Write-Output " Resetting services on VMs"; 
+        foreach -parallel ($AzureVM in $AzureVMsToHandle) 
+        { 
+            Write-Output " Resetting AzureVM : [$AzureVM]  " 
+           
+            Get-AzureRmVM | ? {$_.Name -eq $AzureVM} | Invoke-AzureRmVMRunCommand  -CommandId 'RunShellScript' -ScriptPath $ScriptName
+           
+
         } 
+
+        Write-Output "------------------------ Resetting VM END ------------------------"
+
     } 
 }
